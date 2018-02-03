65c7d6183dde02f616e01443e8dcebdea73ed40a
unknown <leidykurniahatika8597@gmail.com>
2018-02-03 18:27:57 +0700

    push pertama kali

---
+++ app/src/main/java/com/tugas/leidy/leidykurniahatika_1202154343_modul1/MainActivity.java
@@ -0,0 +1,49 @@
+package com.tugas.leidy.leidykurniahatika_1202154343_modul1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mMenuMakanan;
+    private EditText mPorsiMakanan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMenuMakanan = (EditText) findViewById(R.id.menu_makanan);
+        mPorsiMakanan = (EditText) findViewById(R.id.porsi_makanan);
+    }
+
+    public void launchEatbus(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String MenuMakanan = mMenuMakanan.getText().toString();
+        String PorsiMakanan = mPorsiMakanan.getText().toString();
+
+        intent.putExtra("Harga", "50000");
+        intent.putExtra("Menu", MenuMakanan);
+        intent.putExtra("Porsi", PorsiMakanan);
+        intent.putExtra("Tempat", "Eatbus");
+
+        startActivity(intent);
+    }
+
+    public void launchAbnormal(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String MenuMakanan = mMenuMakanan.getText().toString();
+        String PorsiMakanan = mPorsiMakanan.getText().toString();
+
+        intent.putExtra("Harga", "30000");
+        intent.putExtra("Menu", MenuMakanan);
+        intent.putExtra("Porsi", PorsiMakanan);
+        intent.putExtra("Tempat", "Abnormal");
+
+        startActivity(intent);
+    }
+}
